29d89ad57a5068844dfe37c102c3a18a0b601a1d
M.S.Poulaei <sadeghpoolaee@gmail.com>
2022-10-10 14:46:02 +0330

    not used parts deleted like up and down arrows and revenue and so forth

--- resources/views/panel/dashboard.blade.php
+++ resources/views/panel/dashboard.blade.php
@@ -13,11 +13,11 @@
 {{--                                    <p class="text-success ms-2 mb-0 font-weight-medium">+3.5%</p>--}}
                                 </div>
                             </div>
-                            <div class="col-3">
-                                <div class="icon icon-box-success ">
-                                    <span class="mdi mdi-arrow-top-right icon-item"></span>
-                                </div>
-                            </div>
+{{--                            <div class="col-3">--}}
+{{--                                <div class="icon icon-box-success ">--}}
+{{--                                    <span class="mdi mdi-arrow-top-right icon-item"></span>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
                         </div>
                         <h6 class="text-muted font-weight-normal">No of Posts</h6>
                     </div>
@@ -33,11 +33,11 @@
 {{--                                    <p class="text-success ms-2 mb-0 font-weight-medium">+11%</p>--}}
                                 </div>
                             </div>
-                            <div class="col-3">
-                                <div class="icon icon-box-success">
-                                    <span class="mdi mdi-arrow-top-right icon-item"></span>
-                                </div>
-                            </div>
+{{--                            <div class="col-3">--}}
+{{--                                <div class="icon icon-box-success">--}}
+{{--                                    <span class="mdi mdi-arrow-top-right icon-item"></span>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
                         </div>
                         <h6 class="text-muted font-weight-normal">Total Views</h6>
                     </div>
@@ -53,11 +53,11 @@
 {{--                                    <p class="text-danger ms-2 mb-0 font-weight-medium">-2.4%</p>--}}
                                 </div>
                             </div>
-                            <div class="col-3">
-                                <div class="icon icon-box-danger">
-                                    <span class="mdi mdi-arrow-bottom-left icon-item"></span>
-                                </div>
-                            </div>
+{{--                            <div class="col-3">--}}
+{{--                                <div class="icon icon-box-danger">--}}
+{{--                                    <span class="mdi mdi-arrow-bottom-left icon-item"></span>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
                         </div>
                         <h6 class="text-muted font-weight-normal">No of Comments</h6>
                     </div>
@@ -73,11 +73,11 @@
 {{--                                    <p class="text-success ms-2 mb-0 font-weight-medium">+3.5%</p>--}}
                                 </div>
                             </div>
-                            <div class="col-3">
-                                <div class="icon icon-box-success ">
-                                    <span class="mdi mdi-arrow-top-right icon-item"></span>
-                                </div>
-                            </div>
+{{--                            <div class="col-3">--}}
+{{--                                <div class="icon icon-box-success ">--}}
+{{--                                    <span class="mdi mdi-arrow-top-right icon-item"></span>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
                         </div>
                         <h6 class="text-muted font-weight-normal">No of Users</h6>
                     </div>
@@ -143,65 +143,65 @@
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-sm-4 grid-margin">
-                <div class="card">
-                    <div class="card-body">
-                        <h5>Revenue</h5>
-                        <div class="row">
-                            <div class="col-8 col-sm-12 col-xl-8 my-auto">
-                                <div class="d-flex d-sm-block d-md-flex align-items-center">
-                                    <h2 class="mb-0">$32123</h2>
-                                    <p class="text-success ms-2 mb-0 font-weight-medium">+3.5%</p>
-                                </div>
-                                <h6 class="text-muted font-weight-normal">11.38% Since last month</h6>
-                            </div>
-                            <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-                                <i class="icon-lg mdi mdi-codepen text-primary ms-auto"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4 grid-margin">
-                <div class="card">
-                    <div class="card-body">
-                        <h5>Sales</h5>
-                        <div class="row">
-                            <div class="col-8 col-sm-12 col-xl-8 my-auto">
-                                <div class="d-flex d-sm-block d-md-flex align-items-center">
-                                    <h2 class="mb-0">$45850</h2>
-                                    <p class="text-success ms-2 mb-0 font-weight-medium">+8.3%</p>
-                                </div>
-                                <h6 class="text-muted font-weight-normal"> 9.61% Since last month</h6>
-                            </div>
-                            <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-                                <i class="icon-lg mdi mdi-wallet-travel text-danger ms-auto"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4 grid-margin">
-                <div class="card">
-                    <div class="card-body">
-                        <h5>Purchase</h5>
-                        <div class="row">
-                            <div class="col-8 col-sm-12 col-xl-8 my-auto">
-                                <div class="d-flex d-sm-block d-md-flex align-items-center">
-                                    <h2 class="mb-0">$2039</h2>
-                                    <p class="text-danger ms-2 mb-0 font-weight-medium">-2.1% </p>
-                                </div>
-                                <h6 class="text-muted font-weight-normal">2.27% Since last month</h6>
-                            </div>
-                            <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-                                <i class="icon-lg mdi mdi-monitor text-success ms-auto"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+{{--        <div class="row">--}}
+{{--            <div class="col-sm-4 grid-margin">--}}
+{{--                <div class="card">--}}
+{{--                    <div class="card-body">--}}
+{{--                        <h5>Revenue</h5>--}}
+{{--                        <div class="row">--}}
+{{--                            <div class="col-8 col-sm-12 col-xl-8 my-auto">--}}
+{{--                                <div class="d-flex d-sm-block d-md-flex align-items-center">--}}
+{{--                                    <h2 class="mb-0">$32123</h2>--}}
+{{--                                    <p class="text-success ms-2 mb-0 font-weight-medium">+3.5%</p>--}}
+{{--                                </div>--}}
+{{--                                <h6 class="text-muted font-weight-normal">11.38% Since last month</h6>--}}
+{{--                            </div>--}}
+{{--                            <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">--}}
+{{--                                <i class="icon-lg mdi mdi-codepen text-primary ms-auto"></i>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div class="col-sm-4 grid-margin">--}}
+{{--                <div class="card">--}}
+{{--                    <div class="card-body">--}}
+{{--                        <h5>Sales</h5>--}}
+{{--                        <div class="row">--}}
+{{--                            <div class="col-8 col-sm-12 col-xl-8 my-auto">--}}
+{{--                                <div class="d-flex d-sm-block d-md-flex align-items-center">--}}
+{{--                                    <h2 class="mb-0">$45850</h2>--}}
+{{--                                    <p class="text-success ms-2 mb-0 font-weight-medium">+8.3%</p>--}}
+{{--                                </div>--}}
+{{--                                <h6 class="text-muted font-weight-normal"> 9.61% Since last month</h6>--}}
+{{--                            </div>--}}
+{{--                            <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">--}}
+{{--                                <i class="icon-lg mdi mdi-wallet-travel text-danger ms-auto"></i>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div class="col-sm-4 grid-margin">--}}
+{{--                <div class="card">--}}
+{{--                    <div class="card-body">--}}
+{{--                        <h5>Purchase</h5>--}}
+{{--                        <div class="row">--}}
+{{--                            <div class="col-8 col-sm-12 col-xl-8 my-auto">--}}
+{{--                                <div class="d-flex d-sm-block d-md-flex align-items-center">--}}
+{{--                                    <h2 class="mb-0">$2039</h2>--}}
+{{--                                    <p class="text-danger ms-2 mb-0 font-weight-medium">-2.1% </p>--}}
+{{--                                </div>--}}
+{{--                                <h6 class="text-muted font-weight-normal">2.27% Since last month</h6>--}}
+{{--                            </div>--}}
+{{--                            <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">--}}
+{{--                                <i class="icon-lg mdi mdi-monitor text-success ms-auto"></i>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
 
 @endsection
