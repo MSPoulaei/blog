4269f1577f261575bad60895d786756e930ff925
M.S.Poulaei <sadeghpoolaee@gmail.com>
2022-10-10 14:45:28 +0330

    not used parts deleted like tags and some nav links

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -73,15 +73,15 @@
                         <a class="nav-link text-body small">{{ $date->format("l, F j, Y") }}</a>
                         {{--                        Monday, January 1, 2045--}}
                     </li>
-                    <li class="nav-item border-right border-secondary">
-                        <a class="nav-link text-body small" href="#">Advertise</a>
-                    </li>
-                    <li class="nav-item border-right border-secondary">
-                        <a class="nav-link text-body small" href="#">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-body small" href="#">Login</a>
-                    </li>
+{{--                    <li class="nav-item border-right border-secondary">--}}
+{{--                        <a class="nav-link text-body small" href="#">Advertise</a>--}}
+{{--                    </li>--}}
+{{--                    <li class="nav-item border-right border-secondary">--}}
+{{--                        <a class="nav-link text-body small" href="#">Contact</a>--}}
+{{--                    </li>--}}
+{{--                    <li class="nav-item">--}}
+{{--                        <a class="nav-link text-body small" href="#">Login</a>--}}
+{{--                    </li>--}}
                 </ul>
             </nav>
         </div>
@@ -312,27 +312,27 @@
                 </form>
                 <!-- Newsletter End -->
 
-                <!-- Tags Start -->
-                <div class="mb-3">
-                    <div class="section-title mb-0">
-                        <h4 class="m-0 text-uppercase font-weight-bold">Tags</h4>
-                    </div>
-                    <div class="bg-white border border-top-0 p-3">
-                        <div class="d-flex flex-wrap m-n1">
-                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Politics</a>
-                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>
-                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Corporate</a>
-                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>
-                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Health</a>
-                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Education</a>
-                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Science</a>
-                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>
-                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Foods</a>
-                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Travel</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Tags End -->
+{{--                <!-- Tags Start -->--}}
+{{--                <div class="mb-3">--}}
+{{--                    <div class="section-title mb-0">--}}
+{{--                        <h4 class="m-0 text-uppercase font-weight-bold">Tags</h4>--}}
+{{--                    </div>--}}
+{{--                    <div class="bg-white border border-top-0 p-3">--}}
+{{--                        <div class="d-flex flex-wrap m-n1">--}}
+{{--                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Politics</a>--}}
+{{--                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>--}}
+{{--                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Corporate</a>--}}
+{{--                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>--}}
+{{--                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Health</a>--}}
+{{--                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Education</a>--}}
+{{--                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Science</a>--}}
+{{--                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>--}}
+{{--                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Foods</a>--}}
+{{--                            <a href="" class="btn btn-sm btn-outline-secondary m-1">Travel</a>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <!-- Tags End -->--}}
             </div>
         </div>
     </div>
